f2e84951962d3a6abc51ba1aaae6e3780021c645
Irfan <irfantorres123@gmail.com>
2023-05-24 06:45:14 +0530

    initialized the basic app
    setup docker compose for production.

---
+++ server/app.js
@@ -0,0 +1,36 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+require("dotenv").config();
+var usersRouter = require("./routes/users");
+
+const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
+var app = express();
+app.use(
+  app.get("NODE_ENV") === "production" ? logger("production") : logger("dev")
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(CLIENT_BUILD_PATH));
+
+app.use("/users", usersRouter);
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("NODE_ENV") === "production" ? {} : err;
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(CLIENT_BUILD_PATH, "index.html"));
+});
+
+module.exports = app;
